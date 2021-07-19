fe723855d042609fdd294221b262d74e0c953065
Ihor Stepanov <64355919+Ingvarstep@users.noreply.github.com>
2021-07-19 07:40:01 -0400

    add ConvE

--- python/dglke/models/general_models.py
+++ python/dglke/models/general_models.py
@@ -25,6 +25,7 @@ Graph Embedding Model
 5. ComplEx
 6. RotatE
 7. SimplE
+8. ConvE
 """
 import os
 import numpy as np
@@ -94,6 +95,8 @@ class InferModel(object):
             self.score_func = RotatEScore(gamma, emb_init)
         elif model_name == 'SimplE':
             self.score_func = SimplEScore()
+        elif model_name == 'ConvE':
+            self.score_func = ConvEScore(entity_dim)
 
     def load_emb(self, path, dataset):
         """Load the model.
@@ -189,7 +192,7 @@ class KEModel(object):
         Global configs.
     model_name : str
         Which KG model to use, including 'TransE_l1', 'TransE_l2', 'TransR',
-        'RESCAL', 'DistMult', 'ComplEx', 'RotatE', 'SimplE'
+        'RESCAL', 'DistMult', 'ComplEx', 'RotatE', 'SimplE', 'ConvE
     n_entities : int
         Num of entities.
     n_relations : int
@@ -263,6 +266,8 @@ class KEModel(object):
             self.score_func = RotatEScore(gamma, self.emb_init)
         elif model_name == 'SimplE':
             self.score_func = SimplEScore()
+        elif model_name == 'ConvE':
+            self.score_func = ConvEScore(entity_dim)
 
         self.model_name = model_name
         self.head_neg_score = self.score_func.create_neg(True)
